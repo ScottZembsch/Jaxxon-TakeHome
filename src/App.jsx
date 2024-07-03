361cde979ca59db61bbf1efc1604332605421c23
Scott Zembsch <Scottzembsch@gmail.com>
2024-07-03 16:24:18 -0700

    init setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">this is a test</div>
+    </>
+  )
+}
+
+export default App
